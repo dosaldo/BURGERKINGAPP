e12679ec3f5308ece079cd7b525b8ffcefda1f73
dosaldo <99114391+dosaldo@users.noreply.github.com>
2022-06-12 20:35:12 -0600

    Create loing.dart

---
+++ loing.dart
@@ -0,0 +1,191 @@
+import '../bkkingflutter/botones.dart';
+import '../bkkingflutter/temas.dart';
+import '../bkkingflutter/herramientas.dart';
+import '../bkkingflutter/widgets.dart';
+import 'inicio.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key key}) : super(key: key);
+
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  TextEditingController emailAddressController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailAddressController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: Color(0xFFF5EBDC),
+          automaticallyImplyLeading: false,
+          flexibleSpace: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                      child: botones(
+                        borderColor: Colors.transparent,
+                        borderRadius: 30,
+                        borderWidth: 1,
+                        buttonSize: 50,
+                        icon: Icon(
+                          Icons.arrow_back_rounded,
+                          color: temasburguerking.of(context).primaryText,
+                          size: 24,
+                        ),
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(24, 0, 0, 0),
+                child: Text(
+                  'Inicia/Registrate',
+                  style: temasburguerking.of(context).title1.override(
+                        fontFamily: 'Poppins',
+                        color: Color(0xFF4D2008),
+                        fontSize: 32,
+                      ),
+                ),
+              ),
+            ],
+          ),
+          actions: [],
+          elevation: 0,
+        ),
+      ),
+      backgroundColor: Color(0xFFF5EBDC),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(20, 4, 20, 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+                    child: Text(
+                      'Solo necesitamos tu correo para boletines, información o método de contacto.',
+                      style: temasburguerking.of(context).bodyText2.override(
+                            fontFamily: 'Poppins',
+                            color: Color(0xFF4D2008),
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(24, 4, 24, 0),
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                color: temasburguerking.of(context).secondaryBackground,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5,
+                    color: Color(0x4D101213),
+                    offset: Offset(0, 2),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                controller: emailAddressController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'Correo Electrónico',
+                  labelStyle: temasburguerking.of(context).bodyText2,
+                  hintText: 'Ingresa tu correo',
+                  hintStyle: temasburguerking.of(context).bodyText1.override(
+                        fontFamily: 'Lexend Deca',
+                        color: Color(0xFF57636C),
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0x00000000),
+                      width: 0,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                  fillColor: temasburguerking.of(context).secondaryBackground,
+                  contentPadding:
+                      EdgeInsetsDirectional.fromSTEB(24, 24, 20, 24),
+                ),
+                style: temasburguerking.of(context).bodyText1,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+            child: btnwidget(
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InicioWidget(),
+                  ),
+                );
+              },
+              text: 'Entrar',
+              options: btnking(
+                width: 270,
+                height: 50,
+                color: Color(0xFF4D2008),
+                textStyle: temasburguerking.of(context).subtitle2.override(
+                      fontFamily: 'Poppins',
+                      color: temasburguerking.of(context).secondaryBackground,
+                    ),
+                elevation: 3,
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                  width: 1,
+                ),
+                borderRadius: 8,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
